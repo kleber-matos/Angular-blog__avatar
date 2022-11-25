78ade9e42efbf55e5390b215b8194d0afe101a86
kleber-matos <Klebermatos2019@icloud.com>
2022-11-24 22:13:51 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TitleComponent } from './components/title/title.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+    TitleComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
