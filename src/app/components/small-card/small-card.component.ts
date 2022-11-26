a360601a5b295000ae07dbb65975c79486b1146e
kleber-matos <Klebermatos2019@icloud.com>
2022-11-26 16:32:26 -0300

    modificação alert

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -14,12 +14,18 @@ export class SmallCardComponent {
   description: string= ''
 
 
-  verfilme(){
+  github: string= 'https://github.com/kleber-matos/Angular-blog__avatar'
 
-    alert( 
+  
+  texto:String = "Veja o GitHub do projeto: \n" + this.github;
+  verfilme(){
+  
+    //alert(this.texto);
+    
+   window.alert( 
     `${this.title}\n${this.description}`
     )
-    
+    confirm(`Não esqueça de ver no github\n${this.github}`)
   }
 
 }
