f00d0fe1430840291195e0dc9c525963c195f273
kleber-matos <Klebermatos2019@icloud.com>
2022-11-28 21:06:43 -0300

    trabalhando a pagina content

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class ContentComponent {
   photo: string = 'https://midias.correiobraziliense.com.br/_midias/jpg/2022/04/28/675x450/1_avatar_2_the_way_of_water_750x422-7860855.jpg?20220428103146?20220428103146'
   title: string = 'title'
-  description: string = 'descripition!!!'
+  description: string = `descripition!`
 }
